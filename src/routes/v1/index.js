e8a612d839022c6b42efdcdc822e4dcbfaa2518f
muthamizhyadav <muthamizhyadav@gmail.com>
2023-10-25 11:32:59 +0530

    asf

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,7 +3,7 @@ const authRoute = require('./auth.route');
 const userRoute = require('./user.route');
 const docsRoute = require('./docs.route');
 const config = require('../../config/config');
-
+const agoraRoute = require('./AgoraAppId.route')
 const router = express.Router();
 
 const defaultRoutes = [
@@ -14,6 +14,10 @@ const defaultRoutes = [
   {
     path: '/users',
     route: userRoute,
+  },
+  {
+    path: '/agora',
+    route: agoraRoute,
   },
 ];
 
