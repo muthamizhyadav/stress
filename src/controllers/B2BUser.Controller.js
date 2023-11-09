2e020821f9ca86237fdd1801a66672e2e0eb9dfa
bharathiuyarchi <bharathi@uyarchi.com>
2023-11-09 16:10:12 +0530

    d

--- src/controllers/B2BUser.Controller.js
+++ src/controllers/B2BUser.Controller.js
@@ -2,6 +2,20 @@ const catchAsync = require('../utils/catchAsync');
 const b2bUsersService = require('../services/B2BUsers.service');
 const tokenService = require('../services/token.service');
 
+
+const createB2bUsers = catchAsync(async (req, res) => {
+  const users = await b2bUsersService.createUser(req.body);
+  if (!users) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'users Not Fount');
+  }
+  await ManageSalary.createManageSalary({
+    userid: users.id,
+    salary: req.body.salary,
+  });
+  res.status(httpStatus.CREATED).send(users);
+});
+
+
 const B2bUsersLogin = catchAsync(async (req, res) => {
   const users = await b2bUsersService.UsersLogin(req.body);
   const tokens = await tokenService.generateAuthTokens(users);
@@ -9,8 +23,15 @@ const B2bUsersLogin = catchAsync(async (req, res) => {
 });
 
 
-module.exports = {
+const getAllUsers = catchAsync(async (req, res) => {
+  const user = await b2bUsersService.getAllUsers(req.params.page);
+  res.send(user);
+});
 
+
+module.exports = {
+  createB2bUsers,
   B2bUsersLogin,
+  getAllUsers
 
 };
--- src/services/B2BUsers.service.js
+++ src/services/B2BUsers.service.js
@@ -3,6 +3,13 @@ const { Users } = require('../models/B2Busers.model');
 const ApiError = require('../utils/ApiError');
 
 
+
+const createUser = async (userBody) => {
+  let value = Users.create(userBody);
+  return value;
+};
+
+
 const UsersLogin = async (userBody) => {
   const { phoneNumber, password } = userBody;
   let userName = await Users.findOne({ phoneNumber: phoneNumber });
@@ -10,7 +17,6 @@ const UsersLogin = async (userBody) => {
     throw new ApiError(httpStatus.UNAUTHORIZED, 'Phone Number Not Registered');
   } else {
     if (await userName.isPasswordMatch(password)) {
-      //console.log('Password Macthed');
     } else {
       throw new ApiError(httpStatus.UNAUTHORIZED, "Passwoed Doesn't Match");
     }
@@ -18,7 +24,91 @@ const UsersLogin = async (userBody) => {
   return userName;
 };
 
+
+const getAllUsers = async (page) => {
+  let values = await Users.aggregate([
+    {
+      $lookup: {
+        from: 'roles',
+        localField: 'userRole',
+        foreignField: '_id',
+        pipeline: [
+          {
+            $project: {
+              roleName: 1,
+            },
+          },
+        ],
+        as: 'RoleData',
+      },
+    },
+    {
+      $unwind: '$RoleData',
+    },
+    {
+      $lookup: {
+        from: 'musers',
+        localField: '_id',
+        foreignField: 'user_id',
+        as: 'metadatas',
+      },
+    },
+    {
+      $project: {
+        _id: 1,
+        name: 1,
+        email: 1,
+        phoneNumber: 1,
+        active: 1,
+        salary: 1,
+        dateOfJoining: 1,
+        stepTwo: 1,
+        createdAt: 1,
+        userrole: '$RoleData.roleName',
+        metavalue: '$metadatas',
+      },
+    },
+    {
+      $skip: 10 * page,
+    },
+    {
+      $limit: 10,
+    },
+  ]);
+  let total = await Users.aggregate([
+    {
+      $lookup: {
+        from: 'roles',
+        localField: 'userRole',
+        foreignField: '_id',
+        pipeline: [
+          {
+            $project: {
+              roleName: 1,
+            },
+          },
+        ],
+        as: 'RoleData',
+      },
+    },
+    {
+      $unwind: '$RoleData',
+    },
+    {
+      $lookup: {
+        from: 'musers',
+        localField: '_id',
+        foreignField: 'user_id',
+        as: 'metadatas',
+      },
+    },
+  ]);
+  return { values: values, total: total.length };
+};
+
 module.exports = {
+  createUser,
   UsersLogin,
-  
+  getAllUsers
+
 };
