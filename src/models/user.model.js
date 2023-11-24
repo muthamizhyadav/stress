8e1b60784580d2127735ec8de2c31824907aa389
bharathiuyarchi <bharathi@uyarchi.com>
2023-11-24 17:44:34 +0530

    s

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -8,37 +8,17 @@ const userSchema = mongoose.Schema(
   {
     name: {
       type: String,
-      required: true,
-      trim: true,
     },
     email: {
       type: String,
-      required: true,
-      unique: true,
-      trim: true,
-      lowercase: true,
-      validate(value) {
-        if (!validator.isEmail(value)) {
-          throw new Error('Invalid email');
-        }
-      },
+
     },
     password: {
       type: String,
-      required: true,
-      trim: true,
-      minlength: 8,
-      validate(value) {
-        if (!value.match(/\d/) || !value.match(/[a-zA-Z]/)) {
-          throw new Error('Password must contain at least one letter and one number');
-        }
-      },
       private: true, // used by the toJSON plugin
     },
     role: {
       type: String,
-      enum: roles,
-      default: 'user',
     },
     isEmailVerified: {
       type: Boolean,
--- src/models/userDetails.model.js
+++ src/models/userDetails.model.js
@@ -15,7 +15,7 @@ const userSchema = new mongoose.Schema(
       type: String,
     },
     mobileNumber: {
-      type: Number,
+      type: String,
     },
     email: {
       type: String,
