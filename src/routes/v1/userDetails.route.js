494f9c24e1d174a461b0f2ddaac0c3e3e6c1640b
muthamizhyadav <muthamizhyadav@gmail.com>
2024-01-08 15:41:45 +0530

    adf

--- src/routes/v1/userDetails.route.js
+++ src/routes/v1/userDetails.route.js
@@ -2,13 +2,13 @@ const express = require('express');
 const userDetailsController = require('../../controllers/userDetails.controller');
 const router = express.Router();
 
-const { UserAuth, verifyOTP } = require("../../controllers/userauth.controller")
+const { UserAuth, verifyOTP } = require('../../controllers/userauth.controller');
 
-const multer = require("multer");
+const multer = require('multer');
 const storage = multer.memoryStorage({
-    destination: function (req, res, callback) {
-        callback(null, '');
-    },
+  destination: function (req, res, callback) {
+    callback(null, '');
+  },
 });
 const profile = multer({ storage }).single('profile');
 router.route('/user/profile/upload').put(UserAuth, profile, userDetailsController.upload_image_profile);
@@ -18,12 +18,8 @@ router.route('/verfiy/otp').post(verifyOTP, userDetailsController.verify_otp);
 router.route('/verfiy/otp').get(verifyOTP, userDetailsController.verify_otp_get);
 router.route('/resend/otp').post(userDetailsController.verify_mobile_number);
 
-
 router.route('/user/deatils').get(UserAuth, userDetailsController.get_user_deatils);
 router.route('/user/deatils').post(UserAuth, userDetailsController.update_user_deatils);
-
-
-
-
+router.route('/manage/Clients').get(userDetailsController.manage_Clients);
 
 module.exports = router;
