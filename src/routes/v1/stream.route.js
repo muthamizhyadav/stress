dccc97ca7e78103eebc800da883a3f033091f814
Bharathi <bharathi@Bharathis-MBP.domain.name>
2024-01-11 17:24:45 +0530

    D

--- src/routes/v1/stream.route.js
+++ src/routes/v1/stream.route.js
@@ -8,7 +8,7 @@ const { CounsellorAuth } = require('../../controllers/Counsellorauth.controller'
 
 router.route('/create/stream').post(UserAuth, streamrequerst.create_stream_request);
 router.route('/get/stream').get(CounsellorAuth, streamrequerst.get_stream_details);
-router.route('/s').get(UserAuth, streamrequerst.get_stresscall_details_request);
+router.route('/get/stresscall/details').get(UserAuth, streamrequerst.get_stresscall_details_request);
 router.route('/connect/consellor').post(CounsellorAuth, streamrequerst.connect_counsellor_request);
 router.route('/counseller/disconnect/consellor').post(CounsellorAuth, streamrequerst.disconnect_counsellor_request);
 router.route('/connect/consellor').get(CounsellorAuth, streamrequerst.get_connect_counsellor_request);
