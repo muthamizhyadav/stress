0c7bca0bffe44f2d7a0eac3ee7d1f745b2791ff5
bharathidev0303 <bharathiraja996574@gmail.com>
2024-02-09 17:14:03 +0530

    D

--- src/services/stream.service.js
+++ src/services/stream.service.js
@@ -1737,9 +1737,9 @@ const inform_user_neighbour = async (req) => {
     throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
   }
   // let otp = await userserive.Otp(user.neighbourContact, user._id);
-  let inform = await Informadmin.findOne({ streamId: stream._id, user: stream.userId, counsellerID: req.userId });
+  let inform = await Informadmin.findOne({ streamId: stream._id, userId: stream.userId, counsellerID: req.userId });
   if (!inform) {
-    inform = await Informadmin.create({ streamId: stream._id, user: stream.userId, counsellerID: req.userId, neighbour: true });
+    inform = await Informadmin.create({ streamId: stream._id, userId: stream.userId, counsellerID: req.userId, neighbour: true });
   }
   if (!inform.neighbour) {
     inform.neighbour = true;
