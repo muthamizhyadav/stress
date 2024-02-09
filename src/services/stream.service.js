f5c2fece36315072ad4846fad3c800cb64378cd3
bharathidev0303 <bharathiraja996574@gmail.com>
2024-02-09 17:29:34 +0530

    D

--- src/services/stream.service.js
+++ src/services/stream.service.js
@@ -1760,9 +1760,9 @@ const inform_user_immediate = async (req) => {
     throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
   }
 
-  let inform = await Informadmin.findOne({ streamId: stream._id, userId: stream.userId, counsellerID: req.userId });
+  let inform = await Informadmin.findOne({ streamId: stream._id, userId: stream.userId, counsellerID: req.userId , streamTimeLine: stream.streamTimeline});
   if (!inform) {
-    inform = await Informadmin.create({ streamId: stream._id, userId: stream.userId, counsellerID: req.userId, immediate: true });
+    inform = await Informadmin.create({ streamId: stream._id, userId: stream.userId, counsellerID: req.userId, immediate: true , streamTimeLine: stream.streamTimeline});
   }
   if (!inform.immediate) {
     inform.immediate = true;
